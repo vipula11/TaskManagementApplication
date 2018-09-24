f26fa0a746522424c24be004a26c1963078d356c
vipula11 <vipula.rohini@gmail.com>
2018-09-23 22:22:15 -0600

    Latest

--- TaskManagementApplication/AngularScripts/App.js
+++ TaskManagementApplication/AngularScripts/App.js
@@ -1,20 +1,3 @@
 ﻿var taskManagementApp = angular.module('taskManagementApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
 
 console.log("App");
-
-taskManagementApp.config(function ($routeProvider, $locationProvider) {
-    console.log('route');
-    $routeProvider.
-        when('/', {
-            templateUrl: '/htmlPages/createDashboard.html',
-            controller: 'homeController',
-        })
-
-});
-
-taskManagementApp.controller('homeController', ['$scope', '$location', '$http', '$route', function ($scope, $location, $http, $route) {
-
-    
-
-
-}]);
